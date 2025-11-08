65c230b3b5df8ad71ef14949729a9dd96b43c7e1
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-07 20:29:57 -0500

    clase figura  ejercicio 1

---
+++ src/protocoloindividual_3/Rectangulo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package protocoloindividual_3;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Rectangulo extends Figura {
+    private double base;
+    private double altura;
+    
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return base * altura;
+    }
+}
