ed4f5d6fdd58eea48cc585f7dab35bc74e97c6bc
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-11-12 18:09:12 -0500

    clase de prueba vehiculo error ejercicio 3

---
+++ src/protocoloindividual_3/claseVehiculoError.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package protocoloindividual_3;
+
+/**
+ *
+ * @author Sebastian
+ */
+
+public class claseVehiculoError {
+    public static void main(String[] args) {
+        
+        // ❌ ERROR: No se puede instanciar una clase abstracta
+        Vehiculo v = new Vehiculo("Toyota");
+
+        v.mostrarMarca();
+        v.acelerar();
+    }
+}
